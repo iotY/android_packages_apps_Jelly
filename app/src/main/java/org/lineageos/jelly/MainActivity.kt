58aa0c21d357db1f527812f8f3acf319e2161d6c
Luca Stefani <luca.stefani.ge1@gmail.com>
2024-05-05 09:59:20 +0000

    Jelly: Fix-up incognito condition
    Oops
    
    Change-Id: I9a5378ebef95ee197636583cffc35eb18fd21eb3

--- app/src/main/java/org/lineageos/jelly/MainActivity.kt
+++ app/src/main/java/org/lineageos/jelly/MainActivity.kt
@@ -292,7 +292,7 @@ class MainActivity : WebViewExtActivity(), SharedPreferences.OnSharedPreferenceC
         }
 
         CookieManager.getInstance()
-            .setAcceptCookie(incognito && sharedPreferencesExt.cookiesEnabled)
+            .setAcceptCookie(!incognito && sharedPreferencesExt.cookiesEnabled)
 
         webView.init(this, urlBarLayout, incognito)
         webView.isDesktopMode = desktopMode
